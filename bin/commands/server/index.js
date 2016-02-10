e50ea6021b0fe832de887551355499855eb5297d
Howard.Zuo <leftstick@qq.com>
2016-02-10 16:21:40 +0800

    new command server

---
+++ bin/commands/server/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var path = require('path');
+var browserSync = require('browser-sync');
+var history = require('connect-history-api-fallback');
+
+var cmd = {
+    command: 'server',
+    description: 'serve current repo as web server',
+    options: [
+        {
+            flags: '-p, --port <port>',
+            description: 'use a specific port for the server'
+        },
+        {
+            flags: '-m, --html5',
+            description: 'enable html5 mode, which respond with index.html for 404 request'
+        }
+    ],
+    precheck: function() {
+        return true;
+    },
+    action: function(options, cb) {
+        var bs = browserSync.create();
+        var initOpts = {
+            ui: false,
+            server: {
+                baseDir: process.cwd(),
+                directory: false
+            },
+            files: [
+                './**/*.*'
+            ],
+            port: options.port || 3000,
+            open: 'local',
+            reloadOnRestart: true,
+            injectChanges: true
+        };
+        if (options.html5) {
+            initOpts.middleware = [history()];
+        }
+
+        bs.init(initOpts);
+        bs.reload();
+    }
+};
+
+module.exports = cmd;
--- docs/api.md
+++ docs/api.md
@@ -31,4 +31,21 @@ Lint your JavaScript source code.
 
 * `filePath`: Can be either explicit file path, or [glob-pattern](https://github.com/isaacs/node-glob#glob-primer)
 
+>You have to make sure the `glob-pattern` to be quoted if you use it.
+
 ![](./img/lint.png)
+
+### server ###
+
+```bash
+movoto server [options]
+```
+
+**server options**
+
+* `-p`, `--port`: Use a specific port for the server
+* `-m`, `--html5`: Enable html5 mode, which respond with index.html for 404 request
+
+Serve current repo as web server.
+
+![](./img/server.png)
