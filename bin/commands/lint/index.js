b616634954b6c5cbeddb6363976a7cd9971655e2
Howard.Zuo <leftstick@qq.com>
2016-02-15 13:13:44 +0800

    update eslintrc

--- bin/commands/lint/index.js
+++ bin/commands/lint/index.js
@@ -18,27 +18,29 @@ var cmd = {
             description: 'lint not staged files'
         }
     ],
-    precheck: function() {
+    precheck: function(){
         return true;
     },
-    action: function(fileGlob, options, cb) {
+    action: function(fileGlob, options, cb){
 
         var cli = new CLIEngine({
             configFile: path.resolve(__dirname, 'eslintrc.json'),
             useEslintrc: false
         });
-        var filesThen;
-        if (fileGlob) {
+        var filesThen,
+            ERROR = 'Incorrect usage, you have to set either fileGlob or --unstaged option';
+
+        if (fileGlob){
             filesThen = promiseify(glob)(fileGlob, {
                 cwd: process.cwd()
             });
-        } else if (options.unstaged) {
+        }else if (options.unstaged){
             filesThen = promiseify(shell)(['git status --short'], {
                 displayCmdItself: false,
                 displayCmdResult: false
             })
-                .then(function(data) {
-                    if (!data || !data.length || !data[0]) {
+                .then(function(data){
+                    if (!data || !data.length || !data[0]){
                         return [];
                     }
                     return data[0].split('M')
@@ -46,13 +48,14 @@ var cmd = {
                         .map((file) => file.trim().replace('\n', ''))
                         .filter((file) => path.extname(file) === '.js');
                 });
-        } else {
-            filesThen = Promise.reject(new Error('Incorrect usage, you have to set either fileGlob or --unstaged option'));
+        }else {
+
+            filesThen = Promise.reject(new Error(ERROR));
         }
         filesThen
-            .then(function(files) {
-                if (!files || !files.length) {
-                    return log(chalk.yellow('no matched file found!'));;
+            .then(function(files){
+                if (!files || !files.length){
+                    return log(chalk.yellow('no matched file found!'));
                 }
                 var report = cli.executeOnFiles(files);
                 var formatter = CLIEngine.getFormatter('stylish');
